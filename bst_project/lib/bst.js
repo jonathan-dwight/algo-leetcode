442015e58bdf3dcc2a0cf7738413e200711291aa
jonathan-dwight <jd.buendia0711@gmail.com>
2020-12-13 20:28:57 -0800

    add trie project

--- bst_project/lib/bst.js
+++ bst_project/lib/bst.js
@@ -8,7 +8,30 @@ class TreeNode {
 
 
 class BST {
-   
+    constructor() {
+        this.root = null
+    }
+
+    insert(val, root=this.root) {
+        if (!this.root) {
+            this.root = new TreeNode(val);
+            return
+        }
+
+        if(val < root.val) {
+            if (!root.left) {
+                root.left = new TreeNode(val);
+            } else {
+                this.insert(val, root.left)
+            }
+        } else {
+            if (!root.right) {
+                root.right = new TreeNode(val);
+            } else {
+                this.insert(val, root.right)
+            }
+        }
+    }
 }
 
 module.exports = {
