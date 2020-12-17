51a300fd817374ad3de08f50e696dbdda7eb7263
jonathan-dwight <jd.buendia0711@gmail.com>
2020-12-16 17:19:50 -0800

    add backtobackSWE problems

--- leetcode/min-set-size.js
+++ leetcode/min-set-size.js
@@ -26,3 +26,23 @@ var minSetSize = function (arr) {
     }
     return ans;
 };
+
+
+var rotate = function (matrix) {
+    let size = matrix.length - 1;
+
+    for (let layer = 0; layer < Math.floor(matrix.length / 2); layer++) {
+        for (let i = layer; i < size - layer; i++) {
+            let top = matrix[layer][i];
+            let right = matrix[i][size - layer];
+            let bottom = matrix[size - layer][size - i];
+            let left = matrix[size - i][layer];
+
+            matrix[layer][i] = left;
+            matrix[i][size - layer] = top;
+            matrix[size - layer][size - i] = right;
+            matrix[size - i][layer] = bottom;
+        }
+    }
+    return matrix
+};
