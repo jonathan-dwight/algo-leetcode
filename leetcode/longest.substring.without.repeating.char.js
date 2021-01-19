49fb6da28c2c338e19d73ca770730d81d266d379
jonathan-dwight <jd.buendia0711@gmail.com>
2021-01-18 17:29:45 -0800

    add Binary Tree Right Side View

--- leetcode/longest.substring.without.repeating.char.js
+++ leetcode/longest.substring.without.repeating.char.js
@@ -24,3 +24,19 @@ var lengthOfLongestSubstring = function (s) {
     if (!ans) ans = check.size;
     return ans;
 };
+
+// o(n) - solution on LeetCode;
+
+    var start = 0, maxLen = 0;
+    var map = new Map();
+
+    for (var i = 0; i < s.length; i++) {
+        var ch = s[i];
+
+        if (map.get(ch) >= start) start = map.get(ch) + 1;
+        map.set(ch, i);
+
+        if (i - start + 1 > maxLen) maxLen = i - start + 1;
+    }
+
+    return maxLen;
